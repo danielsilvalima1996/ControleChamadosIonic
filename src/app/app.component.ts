3b5e6ce1f83721c1d49dd028e0f321cf902557e8
Daniel da Silva de Lima <dsl15021996@gmail.com>
2019-11-04 17:27:02 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
+      title: 'Dashboard',
       url: '/home',
       icon: 'home'
     },
     {
-      title: 'List',
+      title: 'Chamados',
       url: '/list',
+      icon: 'list'
+    },
+    {
+      title: 'Portal Web',
       icon: 'list'
     }
   ];
